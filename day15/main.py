7e2b66633d1abfa4fd74d934b7c63d83aef37469
bence <75566095+31b4@users.noreply.github.com>
2022-12-15 09:38:51 +0100

    main.py

--- day15/main.py
+++ day15/main.py
@@ -1,3 +1,27 @@
 l = open("input.txt").read().splitlines()
 
-print(l)
+def ReadSave():
+    sensors = []
+    beacons= []
+    grid={}
+    for line in l:
+        sx, sy, bx, by = map(int, re.findall(r'(?<=\=)(.*?)(?=,|\:|\n)', line))
+        sensors.append((sx,sy))
+        beacons.append((bx,by))
+        grid[(sx,sy)]= 'S'
+        grid[(bx,by)]= 'B'
+    return sensors,beacons
+
+def DrawGrid(grid):
+    clear()
+    for y in range(100):
+        for x in range(330, 680):
+            if grid.get((x,y)) == 'o':
+                print("o",end="")
+            else:
+                print("#" if (x, y) in grid else ".", end="")
+        print()
+    print()
+
+sensors,beacons,grid = ReadSave()
+
